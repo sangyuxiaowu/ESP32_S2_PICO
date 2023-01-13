1c4c5ef1aad631cdccf8041a75a9e573a41f4292
sang <sang93@qq.com>
2023-01-13 14:28:37 +0800

    板载LED ESP32 WS2812 Driver

--- BlinkBoardLed/Program.cs
+++ BlinkBoardLed/Program.cs
@@ -1,8 +1,6 @@
 
-using nanoFramework.Hardware.Esp32;
-using System;
-using System.Device.Pwm;
 using System.Threading;
+using BlinkBoardLed.WS2812;
 
 namespace BlinkBoardLed
 {
@@ -12,43 +10,55 @@ namespace BlinkBoardLed
 
         public static void Main()
         {
-            // 先设置 LED 
-            Configuration.SetPinFunction(9, DeviceFunction.PWM1);
-            PwmChannel led = PwmChannel.CreateFromPin(9, 40000, 0);
-            led.Start();
+            // ESP32 WS2812 Driver
+            // https://github.com/nanoframework/nf-Community-Contributions/tree/master/drivers/ESP32-WS2812
 
-            bool goingUp = true;
-            float dutyCycle = .00f;
+            // 微雪的 ESP32-S2-Pico 只有一个灯珠
 
-            while (true)
+            // 生成多少种颜色
+            uint ledCount = 25;
+
+            PixelController controller = new PixelController(9, ledCount, false);
+
+            
+            // 简单测试颜色
+            controller.SetColor(0,255,0,0);
+            controller.UpdatePixels();
+            Thread.Sleep(1000);
+            controller.SetColor(0, 0, 255, 0);
+            controller.UpdatePixels();
+            Thread.Sleep(1000);
+            controller.SetColor(0, 0, 0, 255);
+            controller.UpdatePixels();
+            Thread.Sleep(1000);
+            controller.SetColor(0, 255, 255, 255);
+            controller.UpdatePixels();
+            Thread.Sleep(1000);
+
+
+
+            // 添加开始时设置的 ledCount 种颜色
+            int step = (int)(360 / ledCount);
+            var hue = 0;
+            for (uint i = 0; i < ledCount; i++)
             {
-                if (goingUp)
-                {
-                    // slowly increase light intensity
-                    dutyCycle += 0.05f;
-
-                    // change direction if reaching maximum duty cycle (100%)
-                    if (dutyCycle > .95)
-                        goingUp = !goingUp;
-                }
-                else
-                {
-                    // slowly decrease light intensity
-                    dutyCycle -= 0.05f;
-
-                    // change direction if reaching minimum duty cycle (0%)
-                    if (dutyCycle < 0.10)
-                        break;
-                }
-
-                // update duty cycle
-                led.DutyCycle = dutyCycle;
-
-                Thread.Sleep(50);
+                // HSV
+                // 色调H 取值范围为0°～360°
+                // 饱和度S 取值范围为0%～100%，值越大，颜色越饱和
+                // 明度V 表示颜色明亮的程度,通常取值范围为0%（黑）到100%（白）
+                // V 这个取值挺好的，设置为1 差点亮瞎
+                controller.SetHSVColor((short)i, (short)hue, 1, 0.05f);
+                hue = hue + step;
+                controller.UpdatePixels();
             }
-            led.DutyCycle = .00f;
 
-            led.Stop();
+            // 循环变换颜色
+            for (; ; )
+            {
+                controller.MovePixelsByStep(1);
+                controller.UpdatePixels();
+                Thread.Sleep(500);
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ESP32_S2_PICO
+﻿# ESP32_S2_PICO
+
+.NET nanoFramework 演示 微雪的 ESP32-S2-Pico 使用
+
+# 案例说明
+
+1. SerialPortHello： 串口打印 hello
+2. BlinkBoardLed：板载 RGB 控制，WS2812
+3. RGBLed：外接全彩 RGB 灯的控制 
+
+# 相关文章
+
+1. 《快速入门 .NET nanoFramework 开发 ESP32-Pico 应用》 [https://mp.weixin.qq.com/s/T8NFQbErDSDfZDqzW-5zmA](https://mp.weixin.qq.com/s/T8NFQbErDSDfZDqzW-5zmA)
+
+
+<p align="center">
+相关介绍文章可关注我的微信公众号：桑榆肖物 <br/>
+<img src="https://open.weixin.qq.com/qr/code?username=gh_c874018d0317" />
+</p>
+
+
